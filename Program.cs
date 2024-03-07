9d3b4e0e3ffdc9114e86e051688e2bb0be3cbcf9
Ezra Mallo <ezra.c.mallo@gmail.com>
2024-03-07 13:16:25 -0700

    Added Mutation class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using static GraphQLDemo.Program;
-
 namespace GraphQLDemo
 {
     public class Program
@@ -10,42 +9,55 @@ namespace GraphQLDemo
 
             //Add A graphql sercive
             builder.Services.AddGraphQLServer()
-                                      .AddQueryType<FindStudent>();
-
-
+                                      .AddQueryType<FindStudent>()
+                                      .AddMutationType<Mutation>();
 
             var app = builder.Build();
-
 
             // AddGraghQL Endpoint
             app.UseRouting().UseEndpoints(endpoints => endpoints.MapGraphQL());
 
-
             app.MapGet("/", () => "Hello World!");
-
             app.Run();
-
-
-
         }
         //Build a data record/dataset
-        public record Student(string Name, String Program);
-        public record Instrutor(string Name, String Program);
+        public record Student(string FirstName, string LastName, string Gender, int Age, string Program);
+
+        public record Instrutor(string Name, string Program);
 
         // Build a query
         public class FindStudent
         {
             readonly List<Student> _students = new()
             {
-                new Student ("Nick", "Doctorate"),
-                new Student ("Carlos", "Emeritus")
+                new Student ("Arvind", "Pandit","M", 38, "BAIST-IS"),
+                new Student ("Carlos", "Marquez","M", 27, "BAIST-IS"),
+                new Student ("Evan", "Keefe","M", 25, "BAIST-IS"),
+                new Student ("Ezra", "Mallo","M", 38, "BAIST-IS"),
+                new Student ("Gunooor", "Randhawa","M", 22, "BAIST-IS"),
+                new Student ("Nicholas", "Ekwom","M", 17, "BAIST-IS"),
+                new Student ("Noelyn", "Yamat","F", 28, "BAIST-IS")
             };
             // Build a query
             public List<Student> GetStudents() => _students;
-
+            public List<Student> GetStudentsWithAgeLessThan25() => _students.Where(student => student.Age < 25).ToList();
         }
 
 
+        //buid Mutation
+        public class Mutation
+        {
+            private readonly List<Student> _students = new List<Student>();
+
+            public Student AddNewStudent(string firstName, string lastName, string gender, int age, string program)
+            {
+                var newStudent = new Student(firstName, lastName, gender, age, program);
+                _students.Add(newStudent);
+
+                return newStudent;
+            }
+        }
 
     }
 }
+
